6a3a9b771c4d3fe495a9ba4b0e717c307e3ffbe9
jacegonzales25 <jacegonzales25@gmail.com>
2024-10-03 14:18:24 +0800

    feat: centralized the firebaseConfig

---
+++ vibe-web-application/lib/firebaseConfig.ts
@@ -0,0 +1,20 @@
+import { FirebaseOptions, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const FIREBASE_CONFIGURATION = process.env.FIREBASE_CONFIGURATION || "";
+
+// Parse the Firebase configuration
+const firebaseConfig: FirebaseOptions = JSON.parse(FIREBASE_CONFIGURATION as string);
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore and Firebase Auth services
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, db, auth };
